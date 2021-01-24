ce75471065a67aa991b1d9b588a4ec1248e0b3ab
walterroby <62225850+walterroby@users.noreply.github.com>
2021-01-23 19:42:58 -0800

    Create FollowUpBoss_put_custom_fields.py

---
+++ FollowUpBoss_put_custom_fields.py
@@ -0,0 +1,34 @@
+import requests
+
+# Post Field Values to Custom Fields from your application
+
+person_id = input.get('fub_pid') # input('Enter Follow Up Boss PersionID: ')
+
+sys = 'Your-System'
+sys_key = 'Your-System-Key'
+auth = 'Basic YourAuthKey'
+people_url = "https://api.followupboss.com/v1/people/"+str(person_id)
+
+headers = {
+  'X-System': sys,
+  'X-System-Key': sys_key,
+  'Authorization': auth,
+  'Content-Type': 'application/json'
+}
+
+payload = {
+    'customCOEDays' : input.get('customCOEDays'),
+    'customInspectionPeriod' : input.get('customInspectionPeriod'),
+    'customEstAssignmentPrice' : input.get('customEstAssignmentPrice'),
+    'customAssignmentPrice' : input.get('customAssignmentPrice'),
+    'customPurchasePrice' : input.get('customPurchasePrice'),
+    'customUnderContractDate' : input.get('customUnderContractDate'),
+    'customEstimatedRepairs' : input.get('customEstimatedRepairs'),
+    'customEstimatedARV' : input.get('customEstimatedARV'),
+    'customCondition' : input.get('customCondition'),
+    'customCloseDate' : input.get('customCloseDate')
+    }
+
+response = requests.request("PUT", people_url, headers=headers, data = payload)
+
+print(response.text.encode('utf8'))
